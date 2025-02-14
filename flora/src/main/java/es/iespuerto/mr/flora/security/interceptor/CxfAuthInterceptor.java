855f4b40ab401d6a66d6019fd184c9d1b7ec7f38
Melissa Ruiz <125923092+mruizgl@users.noreply.github.com>
2025-02-14 00:40:14 +0000

    docs(cxfAuthInterceptor): se decumenta nueva clase en javadoc

--- flora/src/main/java/es/iespuerto/mr/flora/security/interceptor/CxfAuthInterceptor.java
+++ flora/src/main/java/es/iespuerto/mr/flora/security/interceptor/CxfAuthInterceptor.java
@@ -14,6 +14,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+/**
+ * Interceptor de autenticación para CXF que valida los tokens JWT en los encabezados HTTP.
+ */
 public class CxfAuthInterceptor extends AbstractPhaseInterceptor<Message> {
 
     private static final String ROLE_ADMIN = "ROLE_ADMIN";
@@ -21,15 +24,29 @@ public class CxfAuthInterceptor extends AbstractPhaseInterceptor<Message> {
 
     private JwtUtils jwtUtils;
 
+    /**
+     * Establece la utilidad para manejar JWT.
+     *
+     * @param jwtUtils Utilidad para la gestión de tokens JWT.
+     */
     @Autowired
     public void setJwtUtils(JwtUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
     }
 
+    /**
+     * Constructor que define la fase en la que se ejecutará el interceptor.
+     */
     public CxfAuthInterceptor() {
         super(Phase.PRE_INVOKE);
     }
 
+    /**
+     * Maneja el mensaje validando el token JWT y estableciendo el contexto de seguridad.
+     *
+     * @param message Mensaje entrante de CXF.
+     * @throws Fault Si el token no es válido o no está presente.
+     */
     @Override
     public void handleMessage(Message message) throws Fault {
         Map<String, List<String>> protocolHeaders = (Map<String, List<String>>) message.get(Message.PROTOCOL_HEADERS);
